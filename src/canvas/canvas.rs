fb3316c6ea309f898eae450ba6ad106204c62b97
Victor Dudochkin <dudochkin.victor@gmail.com>
2021-02-22 14:50:02 +0700

    Stubs for backens

--- src/canvas/canvas.rs
+++ src/canvas/canvas.rs
@@ -4,7 +4,7 @@
 
 use crate::color::Color;
 use crate::text::TextAlign;
-use crate::math::MatrixInterface;
+use crate::math::Matrix;
 use super::image::ImageDataInterface;
 use super::style::{
     CanvasGradientInterface,
@@ -19,30 +19,25 @@ pub trait CanvasContext {
     // Properties
 
     // Deprecated always returns 1.0
-    // read-only
     fn get_backing_store_pixel_ratio(&mut self) -> f64;
     fn set_backing_store_pixel_ratio(&mut self, value: f64);
 
-    // read-only, override
     // fn get_canvas(&self) -> CanvasElement;
     
     // fn get_current_transform(&self) -> Box<dyn MatrixInterface>;
-    fn set_current_transform(&mut self, value: impl MatrixInterface);
+    fn set_current_transform(&mut self, value: Matrix);
 
     fn get_direction(&self) -> Direction;
     fn set_direction(&self, value: Direction) -> String;
 
-    // @Creates('String|CanvasGradient|CanvasPattern'), @Returns('String|CanvasGradient|CanvasPattern'), read / write
+    // @Creates('String|CanvasGradient|CanvasPattern'), @Returns('String|CanvasGradient|CanvasPattern')
     // fillStyle: Object;
     // fn get_fill_style(&self) -> Box<CanvasStyle<dyn CanvasGradientInterface, dyn CanvasPatternInterface>>;
-    fn set_fill_style(&mut self, value: CanvasStyle<impl CanvasGradientInterface, impl CanvasPatternInterface>);
+    // fn set_fill_style(&mut self, value: CanvasStyle<impl CanvasGradientInterface, impl CanvasPatternInterface>);
     fn set_fill_style_color(&mut self, value: Color);
-    fn set_fill_style_gradient(&mut self, value: impl CanvasGradientInterface);
-    fn set_fill_style_pattern(&mut self, value: impl CanvasPatternInterface);
-
-
-    
-    // read / write
+    // fn set_fill_style_gradient(&mut self, value: impl CanvasGradientInterface);
+    // fn set_fill_style_pattern(&mut self, value: impl CanvasPatternInterface);
+   
     fn get_filter(&self) -> String;
     fn set_filter(&mut self, value: &str);
     
@@ -55,12 +50,11 @@ pub trait CanvasContext {
     fn get_global_composite_operation(&self) -> String;
     fn set_global_composite_operation(&mut self, value: String);
 
-    // The hash code for this object. [...]
-    // read-only, inherited
+    // The hash code for this object. 
     fn get_hash_code(&self) -> u64;
     fn set_hash_code(&mut self, value: u64);
 
-    // Whether images and patterns on this canvas will be smoothed when this canvas is scaled. [...]
+    // Whether images and patterns on this canvas will be smoothed when this canvas is scaled. 
     // imageSmoothingEnabled
     fn is_image_smoothing_enabled(&self) -> bool;
     fn set_image_smoothing(&mut self, value: bool);
@@ -71,7 +65,7 @@ pub trait CanvasContext {
     fn get_line_cap(&self) -> String;
     fn set_line_cap(&mut self, value: String);
 
-    // @SupportedBrowser(SupportedBrowser.CHROME), @SupportedBrowser(SupportedBrowser.IE, '11'), @SupportedBrowser(SupportedBrowser.SAFARI), @Unstable(), read / write
+    // @SupportedBrowser(SupportedBrowser.CHROME), @SupportedBrowser(SupportedBrowser.IE, '11'), @SupportedBrowser(SupportedBrowser.SAFARI), @Unstable()
     fn get_line_dash_offset(&self) -> f64;
     fn set_line_dash_offset(&mut self, value: f64);
 
@@ -81,12 +75,10 @@ pub trait CanvasContext {
     fn get_line_width(&self) -> f64;
     fn set_line_width(&mut self, value: f64);
 
-    // read / write
     fn get_miter_limit(&self) -> f64;
     fn set_miter_limit(&mut self, value: f64);
 
     // // A representation of the runtime type of the object.
-    // // read-only, inherited
     // runtimeType: Type; // TODO
 
     fn get_shadow_blur(&self) -> f64;
@@ -95,19 +87,17 @@ pub trait CanvasContext {
     fn get_shadow_color(&self) -> String;
     fn set_shadow_color(&mut self, value: String);
 
-    // read / write
     fn get_shadow_offset_x(&self) -> f64;
     fn set_shadow_offset_x(&mut self, value: f64);
 
-    // read / write
     fn get_shadow_offset_y(&self) -> f64;
     fn set_shadow_offset_y(&mut self, value: f64);
 
-    // @Creates('String|CanvasGradient|CanvasPattern'), @Returns('String|CanvasGradient|CanvasPattern'), read / write
-    fn set_stroke_style(&mut self, value: CanvasStyle<impl CanvasGradientInterface, impl CanvasPatternInterface>);
+    // @Creates('String|CanvasGradient|CanvasPattern'), @Returns('String|CanvasGradient|CanvasPattern')
+    // fn set_stroke_style(&mut self, value: CanvasStyle<impl CanvasGradientInterface, impl CanvasPatternInterface>);
     fn set_stroke_style_color(&mut self, value: Color);
-    fn set_stroke_style_gradient(&mut self, value: impl CanvasGradientInterface);
-    fn set_stroke_style_pattern(&mut self, value: impl CanvasPatternInterface);
+    // fn set_stroke_style_gradient(&mut self, value: impl CanvasGradientInterface);
+    // fn set_stroke_style_pattern(&mut self, value: impl CanvasPatternInterface);
 
     fn get_text_align(&mut self) -> TextAlign;
     fn set_text_align(&mut self, value: TextAlign);
@@ -122,14 +112,14 @@ pub trait CanvasContext {
     // anticlockwise: bool = false
     fn arc(&self, x: f64, y: f64, radius: f64, start_angle: f64, end_angle: f64, anticlockwise: bool);
     fn arc_to(&self, x1: f64, y1: f64, x2: f64, y2: f64, radius: f64);
-    fn begin_path(&self, );
+    fn begin_path(&self);
     fn bezier_curve_to(&self, cp1x: f64, cp1y: f64, cp2x: f64, cp2y: f64, x: f64, y: f64);
-    fn clear_hit_regions(&self, );
+    fn clear_hit_regions(&self);
     fn clear_rect(&self, x: f64, y: f64, width: f64, height: f64);
 
     // [path_OR_winding: dynamic, winding: String]
     // fn clip(path_OR_winding: dynamic, winding: String); // TODO:
-    // fn close_path(&self, ); // TODO:
+    // fn close_path(&self); // TODO:
     
     // @Creates('ImageData|=Object')
     // [int? sh_OR_sw, dynamic imageDataColorSettings_OR_sh, Map? imageDataColorSettings]
@@ -144,19 +134,19 @@ pub trait CanvasContext {
     // [Element? element]
     // fn drawFocusIfNeeded(element_OR_path: dynamic, element: Element); // TODO:
     
-    // Draws an image from a CanvasImageSource to this canvas. [...]
+    // Draws an image from a CanvasImageSource to this canvas. 
     // @JSName('drawImage')
     // fn drawImage(source: CanvasImageSource, destX: f64, destY: f64); // TODO:
     
-    // Draws an image from a CanvasImageSource to an area of this canvas. [...]
+    // Draws an image from a CanvasImageSource to an area of this canvas. 
     // @JSName('drawImage')
     // fn drawImageScaled(source: CanvasImageSource, destX: f64, destY: f64, destWidth: f64, destHeight: f64); // TODO:
 
-    // Draws an image from a CanvasImageSource to an area of this canvas. [...]
+    // Draws an image from a CanvasImageSource to an area of this canvas. 
     // @JSName('drawImage')
     // fn drawImageScaledFromSource(source: CanvasImageSource, sourceX: f64, sourceY: f64, sourceWidth: f64, sourceHeight: f64, destX: f64, destY: f64, destWidth: f64, destHeight: f64); // TODO:
 
-    // Draws an image from a CanvasImageSource to an area of this canvas. [...]
+    // Draws an image from a CanvasImageSource to an area of this canvas. 
     // {Rectangle<f64>? sourceRect}
     // fn drawImageToRect(source: CanvasImageSource, destRect: Rectangle<f64>, sourceRect: Rectangle<f64>); // TODO:
     
@@ -166,7 +156,7 @@ pub trait CanvasContext {
     // fn fill(path_OR_winding: dynamic, winding: String); // TODO:
     fn fill_rect(&self, x: f64, y: f64, width: f64, height: f64);
     
-    // Draws text to the canvas. [...]
+    // Draws text to the canvas. 
     // [f64? max_width]
     fn fill_text(&self, text: String, x: f64, y: f64, max_width: f64);
     
@@ -176,9 +166,9 @@ pub trait CanvasContext {
     // fn getImageData(sx: i64, sy: i64, sw: i64, sh: i64) -> ImageData; // TODO:
     
     // @SupportedBrowser(SupportedBrowser.CHROME), @SupportedBrowser(SupportedBrowser.IE, '11'), @SupportedBrowser(SupportedBrowser.SAFARI), @Unstable()
-    fn get_line_dash(&self, ) -> Vec<f64>;
+    fn get_line_dash(&self) -> Vec<f64>;
     
-    fn is_context_lost(&self, ) -> bool;
+    fn is_context_lost(&self) -> bool;
     // [dynamic winding_OR_y, String? winding]
     // fn isPointInPath(path_OR_x: dynamic, x_OR_y: f64, winding_OR_y: dynamic, winding: String) -> bool; // TODO:
 
@@ -193,10 +183,10 @@ pub trait CanvasContext {
     fn quadratic_curve_to(&self, cpx: f64, cpy: f64, x: f64, y: f64);
     fn rect(&self, x: f64, y: f64, width: f64, height: f64);
     fn remove_hit_region(&self, id: String);
-    fn reset_transform(&self, );
-    fn restore();
+    fn reset_transform(&self);
+    fn restore(&self);
     fn rotate(&self, angle: f64);
-    fn save(&self, );
+    fn save(&self);
     fn scale(&self, x: f64, y: f64);
 
     // [Path2D? path]
--- src/math.rs
+++ src/math.rs
@@ -1,3 +1,1 @@
-pub trait MatrixInterface {
-
-}
+pub struct Matrix;
